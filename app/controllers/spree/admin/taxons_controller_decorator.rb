2adb9d6bec372884c01b25e0184d3852fc2aa473
hemanthmraju <kumarhemanth.cs@gmail.com>
2014-07-28 12:58:07 +0530

    sort count on hand implemented

--- app/controllers/spree/admin/taxons_controller_decorator.rb
+++ app/controllers/spree/admin/taxons_controller_decorator.rb
@@ -1,7 +1,7 @@
 module Spree
   module Admin
     class TaxonsController < Spree::Admin::BaseController
-      before_filter :load_taxon, :only => [:sort_products, :update_products]
+      before_filter :load_taxon, :only => [:sort_products, :update_products,:sort_hand_count]
 
       def sort_products
         @products = @taxon.active_products
@@ -10,6 +10,16 @@ module Spree
       def update_products
         products = @taxon.products_taxons
         product_ids_positions = params[:product_positions].split(",").map(&:to_i)
+        product_ids_positions.each_with_index do |id, index|
+          product = products.detect{|p| p.product_id == id }
+          product.update_attributes(:position => index) unless product.nil?
+        end
+        redirect_to sort_products_taxons_path(@taxonomy, @taxon.id), :notice => t(:sort_products_taxons_update_message)
+      end
+
+      def sort_hand_count
+        products = @taxon.products_taxons
+        product_ids_positions = @taxon.products.sort_by { |p| p[:count_on_hand]}.reverse.map(&:id)
         product_ids_positions.each_with_index do |id, index|
           product = products.detect{|p| p.product_id == id }
           product.update_attributes(:position => index) unless product.nil?
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Spree::Core::Engine.routes.draw do
 
   match 'admin/taxonomies/:taxonomy_id/taxons/:id/sort_products/' => 'admin/taxons#sort_products', :as => :sort_products_taxons
   match 'admin/taxonomies/:taxonomy_id/taxons/:id/update_products/' => 'admin/taxons#update_products', :as => :update_products_admin_taxonomy_taxon, :method => :post
+  match 'admin/taxonomies/:taxonomy_id/taxons/:id/sort_hand_count/' => 'admin/taxons#sort_hand_count', :as => :sort_hand_count_admin_taxonomy_taxon, :method => :post
 
 end
